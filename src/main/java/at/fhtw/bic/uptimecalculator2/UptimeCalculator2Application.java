687a19ad6377b19c92c553f35e91cde466e0f11d
Nermina Salkic <ic21b114@technikum-wien.at>
2022-12-21 15:47:09 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/uptimecalculator2/UptimeCalculator2Application.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.uptimecalculator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeCalculator2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeCalculator2Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/uptimecalculator2/UptimeCalculator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.uptimecalculator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeCalculator2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
